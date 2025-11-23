5c8ddd4f95ff21b9bc442790c67b15a0fca8b4a9
SachinyadavAug20 <samtagon777@gmail.com>
2025-11-23 10:06:07 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './output.css'
+
+function App() {
+
+  return (
+    <>
+            <div className='bg-black text-white p-5 text-center'>
+                This is a todolist app
+            </div>
+    </>
+  )
+}
+
+export default App
